7a03b37d2e8af5539b88b1f1e78c127c54316034
Soham Bodhani <sohambodhani994@gmail.com>
2025-09-13 09:22:28 +0530

    updated import in chatbot

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from langchain_groq import ChatGroq
 from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 import os
 import shutil
 
